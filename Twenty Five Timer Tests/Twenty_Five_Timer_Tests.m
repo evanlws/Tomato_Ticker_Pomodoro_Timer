95f6ac5aa3900ff0a776f1c67946584570497108
Evan Lewis <EvanLewis@Evans-Mac-mini.local>
2017-07-14 16:01:40 -0400

    More renaming

---
+++ Twenty Five Timer Tests/Twenty_Five_Timer_Tests.m
@@ -0,0 +1,39 @@
+//
+//  Twenty_Five_Timer_Tests.m
+//  Twenty Five Timer Tests
+//
+//  Created by Evan Lewis on 7/14/17.
+//  Copyright © 2017 Evan Lewis. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Twenty_Five_Timer_Tests : XCTestCase
+
+@end
+
+@implementation Twenty_Five_Timer_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
